0e9022744c046d47afb4998176dfebf1c2bc779b
Jaciubek <slawomir.jaciubek@gmail.com>
2025-03-17 22:50:14 +0100

    first commit

---
+++ src/redux/store.jsx
@@ -0,0 +1,65 @@
+
+import { configureStore } from '@reduxjs/toolkit';
+
+import { combineReducers } from 'redux';
+import { contactsReducer } from './reducers/contactsReducer';
+import filterReducer from './reducers/filterReducer';
+
+
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+  whitelist: ["items"],
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const rootReducer = combineReducers({
+  contacts: persistedContactsReducer,
+  filter: filterReducer
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const store = configureStore({
+  reducer: rootReducer,
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
